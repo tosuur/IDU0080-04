466ca161c27febaefdac1c025c2aef8ae9cbff88
Tony Bigwell <tonis.suurkaev@gmail.com>
2017-05-22 22:16:15 +0300

    Choose best offer

--- OrderClient/src/ttu/idu0080/order/protsess/Test.java
+++ OrderClient/src/ttu/idu0080/order/protsess/Test.java
@@ -19,15 +19,32 @@ public class Test {
 		List<Pakkumine> pakkumised = new ArrayList<Pakkumine>();
 		for (Courier courier : couriers) {
 			pakkumised.add(op.getBestOffer(courier.getEnterprise(), order));
-			//System.out.println(courier.getName() + " - percent: "+ courier.getPercentFromOrder());
+			// System.out.println(courier.getName() + " - percent: "+
+			// courier.getPercentFromOrder());
 		}
-		//Courier courier = op.getBestOffer(order, order);
-		//System.out.println("Best: " + courier.getName() + " - percent: "+ courier.getPercentFromOrder());
-		
-		for (Pakkumine pakkumine : pakkumised) {
-			System.out.println(pakkumine.getOfferId() + "  |  " + pakkumine.getPrice() + "  |  " + pakkumine.getDeliveryTime());
-			System.out.println("--------------------");
+		// Courier courier = op.getBestOffer(order, order);
+		// System.out.println("Best: " + courier.getName() + " - percent: "+
+		// courier.getPercentFromOrder());
+		float headus = (float) ((pakkumised.get(0).getPrice() - order.getPriceTotal()) * 0.01
+				* pakkumised.get(0).getDeliveryTime());
+		Pakkumine parimPakkumine = pakkumised.get(0);
+		Courier parimCourier = couriers.get(0);
+		for (Courier courier : couriers) {
+			for (Pakkumine pakkumine : pakkumised) {
+				if ((float) ((pakkumine.getPrice() - order.getPriceTotal()) * 0.01
+						* pakkumine.getDeliveryTime()) < headus) {
+					headus = (float) ((pakkumine.getPrice() - order.getPriceTotal()) * 0.01
+							* pakkumine.getDeliveryTime());
+					parimPakkumine = pakkumine;
+					parimCourier = courier;
+				}
+			}
 		}
+		System.out.println("--------------------");
+		System.out.println("Parim Pakkumine");
+		System.out.println(parimCourier.getName() + "  |  " + parimPakkumine.getOfferId() + "  |  "
+				+ parimPakkumine.getPrice() + "  |  " + parimPakkumine.getDeliveryTime());
+		System.out.println("--------------------");
 	}
 
 }
